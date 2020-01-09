f72dcf9362fef085eecb72ca09f3b0d85502bcc1
Misha Kharitonov <acermini2012@gmail.com>
2020-01-09 04:37:17 +0300

    five commit

---
+++ homeworkc.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+float sqr(float x){
+  return x*x;
+  	const int n=6, m=9;
+    int i, j, imin=0, imax=0;
+}
+  for j:=1 to n do begin
+    if a[i,j]<=min then 
+     begin b:=true;
+     min :=a[i,j];
+     end;
+    s:=s+a[i,j];
+    end;
+
+{
+	readln;
+	system("pause");
+return 0;
+}
